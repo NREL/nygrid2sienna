de7b18608a64f6321a8bfdba83c6f7a0e7dca96b
Liu, Vivienne <Vivienne.Liu@nrel.gov>
2024-12-17 10:26:50 -0700

    data and files for validation use only

--- src/validation.jl
+++ src/validation.jl
@@ -6,13 +6,18 @@ gen[!, "Generation"] = vec(permutedims(Matrix(thermalgen)))
 gen_m = CSV.read("Data/gen_matlab.csv", DataFrame, header=false)
 
 using MAT
-mpc = matread("Data/resultOPF_20190101_00.mat")
+mpc = matread("Data/resultOPF_20190718_14.mat")
 mpc = mpc["resultOPF"]
+branch = mpc["branch"]
+bus = mpc["bus"]
 dcline = mpc["dcline"]
 gencost = mpc["gencost"]
 ifs = mpc["if"]
 iflims = ifs["lims"]
 generation = mpc["gen"]
+CSV.write("branch_0101.csv", Tables.table(branch), writeheader=false)
+CSV.write("gen_0101.csv", Tables.table(generation), writeheader=false)
+CSV.write("gencost_0718.csv", Tables.table(gencost), writeheader=false)
 # gen_m[!, "Column2"] = vec(generation[:, 2])
 
 gen_m[!, "Column2"] = generation[:, 2]
